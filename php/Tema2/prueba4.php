58f81ef66659f4eed5a7cd9f0632449e4ed674bf
ruben <rcgfregenal@gmail.com>
2025-09-24 21:10:47 +0200

    prueba 5

--- php/Tema2/prueba4.php
+++ php/Tema2/prueba4.php
@@ -1,9 +1,11 @@
 <?php
-//prueba
-for ($i=0; $i < ; $i++) { 
-    # code...
-}
+//Generar un numero aleatorio entre 1-10, y calcular el factorial
 
+for ($i=1; $i <=1; $i++) { 
+    $num= rand(1,10);
+     
+     echo "El numero elegido es: $i y su factorial es: $num <br/>";
 
-
+    }
 ?>
+
---
+++ php/Tema2/prueba5.php
@@ -0,0 +1,43 @@
+<?php
+//Introducir tres numeros aleatorios y presentarlos de forma ordenada
+
+
+$num1=rand(1,10);
+$num2=rand(1,10);
+$num3=rand(1,10);
+$maximo=0;
+$medio=0;
+$minimo=0;
+
+
+//MAXIMO
+if ($num1>$num2 && $num1>$num3) {
+    $maximo=$num1;
+} elseif ($num2>$num1 && $num2>$num3) {
+    $maximo=$num2;
+} else {
+    $maximo=$num3;
+}
+
+//MINIMO
+if ($num1<$num2 && $num1<$num3) {
+    $minimo=$num1;
+} elseif ($num2<$num1 && $num2<$num3) {
+    $minimo=$num2;
+} else{
+    $minimo=$num3;
+}
+
+//MEDIO
+if ($num1>$num2 && $num1<$num3) {
+    $medio=$num1;
+} elseif ($num2>$num1 && $num2<$num3) {
+    $medio=$num2;
+} else{
+    $medio=$num3;
+}
+
+ 
+echo "Los numeros al azar son: $num1 $num2 $num3 <br/>";
+echo "Presentamos los numeros de forma ordenada: $maximo  $medio  $minimo";
+?>
